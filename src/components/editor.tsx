7e090bfd2055bad5920f6fe405cf88e612567022
bybogdan <bgdnvarlamov@gmail.com>
2023-07-27 20:04:13 +0400

    Add texts, add storing best result in lc

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -5,6 +5,8 @@ import { mocks } from '../mocks'
 
 type ResType = ('correct' | 'wrong' | 'idle')[]
 
+const LC_BEST_RESULT = 'BEST_RESULT'
+
 export const Editor = () => {
   const mock = useMemo(
     () =>
@@ -63,12 +65,29 @@ export const Editor = () => {
   useEffect(() => {
     if (isStarted && isFinished) {
       const time = isFinished - isStarted
+
       const wpm = (mock.length / 5 / (time / 1000 / 60)).toFixed(0)
+
       const accuracy =
         (
           (res.current.filter((v) => v === 'correct').length / mock.length) *
           100
         ).toFixed(0) + '%'
+
+      let isNewRecord = false
+
+      const prevBestResult = localStorage.getItem(LC_BEST_RESULT)
+      const bestResult = Math.max(Number(prevBestResult), Number(wpm)).toFixed(
+        0
+      )
+
+      if (prevBestResult) {
+        if (Number(prevBestResult) < Number(wpm)) {
+          isNewRecord = true
+        }
+      }
+
+      localStorage.setItem(LC_BEST_RESULT, bestResult)
 
       toast.custom((t) => (
         <div
@@ -76,16 +95,26 @@ export const Editor = () => {
             t.visible ? 'animate-enter' : 'animate-leave'
           } p-4 max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5 mb-10`}
         >
-          <div className="flex-1 w-0 text-start	">
-            <p className=" font-medium text-gray-900">Your results:</p>
-            <p className="mt-1  text-gray-500">
+          <div className="flex-1 w-0 text-start">
+            {isNewRecord ? (
+              <p className="font-bold text-green-600">🎉 New record! 🎉</p>
+            ) : null}
+            <p className="font-medium text-gray-900">Current results:</p>
+
+            <p className=" text-gray-500">
               words per minute: <b className="text-black">{wpm}</b>
             </p>
-            <p className="mt-1  text-gray-500">
+            <p className=" text-gray-500">
               accuracy: <b className="text-black">{accuracy}</b>
             </p>
+
+            <div className="my-4" />
+            <p className="font-medium text-gray-900">Your best result:</p>
+            <p className=" text-gray-500">
+              words per minute: <b className="text-black">{bestResult}</b>
+            </p>
           </div>
-          <div className="self-center">
+          <div className="self-end">
             <button
               type="button"
               className="mt-2 inline-block rounded bg-neutral-800 px-4 pb-2 pt-2.5 font-medium leading-normal text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-neutral-900 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-neutral-900 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]"
@@ -118,14 +147,14 @@ export const Editor = () => {
         isFinished ? 'opacity-20' : ''
       }`}
     >
-      <div className="flex items-center gap-4">
+      <div className="flex items-start gap-2">
+        <img src="/turtleIcon.png" alt="turtle" className="w-8 h-8" />
         <h1 className="text-4xl text-left">turtletype</h1>
-        {/* ADD TURTLE ICON */}
 
         {!isFinished ? (
           <button
             type="button"
-            className="mt-2 inline-block rounded bg-neutral-800 dark:bg-neutral-200 px-4 pb-2 pt-2.5 font-medium leading-normal text-neutral-50 dark:text-neutral-800 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-neutral-800 dark:hover:bg-neutral-300 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-neutral-800 dark:focus:bg-neutral-300 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 dark:active:bg-neutral-300 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]  dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2) vertical-align: top; text-decoration: inherit; text-wrap: balance;"
+            className="inline-block rounded bg-neutral-800 dark:bg-neutral-200 px-4 pb-2 pt-2.5 font-medium leading-normal text-neutral-50 dark:text-neutral-800 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-neutral-800 dark:hover:bg-neutral-300 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-neutral-800 dark:focus:bg-neutral-300 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 dark:active:bg-neutral-300 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]  dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2) vertical-align: top; text-decoration: inherit; text-wrap: balance;"
             onClick={() => window.location.reload()}
           >
             change text 🔄
